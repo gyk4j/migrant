e550f1acc53453975c75bbbcf57df2f6948b2fd6
gyk <147011991+gyk4j@users.noreply.github.com>
2025-03-04 11:01:36 +0800

    Add functions (#3)

--- migrant.cmd
+++ migrant.cmd
@@ -2,11 +2,22 @@
 
 setlocal
 
+goto :main
+
+:read_config
 for /f "tokens=1,* delims==" %%A in (Migrantfile.txt) do (
   set %%A=%%B
 )
+exit /b 0
 
+:dump_config
 echo Unquoted: %var1%
 echo   Quoted: %var2%
+exit /b 0
+
+:main
+call :read_config
+call :dump_config
+exit /b 0
 
 endlocal
--- migrant.ps1
+++ migrant.ps1
@@ -1,8 +1,19 @@
-foreach($line in Get-Content .\Migrantfile.txt) {
-  #echo $line
-  $key, $value = $line -split '=', 2
-  Set-Variable -Name "$key" -Value "$value"
+function Read-Config {
+  foreach($line in Get-Content .\Migrantfile.txt) {
+    #echo $line
+    $key, $value = $line -split '=', 2
+    Set-Variable -Name "$key" -Value "$value" -Scope script
+  }
 }
 
-Write-Output "Unquoted: $var1"
-Write-Output "  Quoted: $var2"
+function Dump-Config {
+  Write-Output "Unquoted: $var1"
+  Write-Output "  Quoted: $var2"
+}
+
+function Main {
+  Read-Config
+  Dump-Config
+}
+
+Main
